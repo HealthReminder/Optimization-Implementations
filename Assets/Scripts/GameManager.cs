e23ec4464005abee694c425bdff32c529264a966
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-03-14 11:23:01 -0700

    LOD
    Added a LOD example using the default Blender mankee.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     float _timeElapsed = 0.0f;
-    float _nextTrigger = 8f;
+    float _nextShower = 5f;
+    float _nextDelivery = 8f;
     public CircleSpawner OutsideBoxSpawner;
+    public BoxSpawner InsideBoxSpawner;
+    private void Start()
+    {
+    }
     private void Update()
     {
         _timeElapsed += Time.deltaTime;
-        if(_timeElapsed >= _nextTrigger)
+        if (_timeElapsed >= _nextShower)
         {
             Debug.Log("Exterior box spawn activated");
             OutsideBoxSpawner.InstantiateTimed();
-            _nextTrigger += 8;
+            _nextShower += 8;
+        }
+        if (_timeElapsed >= _nextDelivery)
+        {
+            Debug.Log("Interior box spawn activated");
+            InsideBoxSpawner.SpawnBoxes(10);
+            _nextDelivery += 5;
         }
     }
 }
