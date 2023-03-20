c1c6d134601cbd231935cfd83f75205d046b2a42
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-03-19 23:21:20 -0700

    Conveyor Belt
    Added a conveyor belt to move rigidbodies around.

---
+++ Assets/Scripts/Objects/ConveyorBelt.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField] private Vector3 forceDirection; //The direction objects will be moved
+    [SerializeField] private float forceMultiplier; //The direction objects will be moved
+    private void OnCollisionStay(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb)
+        {
+            rb.MovePosition(rb.position + forceDirection * forceMultiplier *Time.deltaTime);
+            rb.angularVelocity = collision.gameObject.GetComponent<Rigidbody>().angularVelocity * 0.7f;
+            //collision.gameObject.GetComponent<Rigidbody>().drag = 0;
+        }
+    }
+}
