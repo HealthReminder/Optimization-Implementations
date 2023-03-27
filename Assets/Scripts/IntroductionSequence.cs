242bfd69dbf227337d318a02c9967df901f7fb8e
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-03-26 20:25:07 -0700

    Level Manager
    Created a Level Manager class to hold the information about the level and the common functions that will be used by other action sequences.

--- Assets/Scripts/IntroductionSequence.cs
+++ Assets/Scripts/IntroductionSequence.cs
@@ -3,62 +3,47 @@ using UnityEngine;
 
 public class IntroductionSequence : MonoBehaviour
 {
-    [SerializeField] private PlayerController _playerController;
-    [SerializeField] private LookAtSmooth _playerLookAt;
-    [SerializeField] private Transform _titleTransform;
-    [SerializeField] private CircleSpawner _circleSpawner;
-    [SerializeField] private BoxSpawner _boxSpawner;
-    [SerializeField] private Lamp _lampAlarm;
-    [SerializeField] private TVScreen _TVScreen;
-    [SerializeField] private ConveyorBelt _ConveyorBelt;
-    [SerializeField] private Lamp[] TitleLights;
-    [SerializeField] private Lamp[] BeltLights;
-    [SerializeField] private Lamp CenterLight;
-    [SerializeField] private Lamp LeverLight;
-    [SerializeField] private GameObject recordPlayer;
-
+    [SerializeField] private LevelManager _levelManager;
     public void Initiate()
     {
         StartCoroutine(StartRoutine());
     }
     IEnumerator StartRoutine()
     {
-        yield return _playerLookAt.LookAtRoutine(_titleTransform,0.5f);
-        foreach (Lamp lamp in TitleLights)
-        {
-            lamp.TurnOn();
-            yield return new WaitForSeconds(1);
-        }
+        // Title
+        yield return _levelManager.PlayerLookAt(_levelManager.titleTransform, 0.5f);
+        yield return _levelManager.ToggleTitleLights(true);
         yield return new WaitForSeconds(1);
-        recordPlayer.SetActive(true);
+        _levelManager.recordPlayer.SetActive(true);
         yield return new WaitForSeconds(1);
-        CenterLight.TurnOn();
+        _levelManager.centerLight.TurnOn();
         yield return new WaitForSeconds(1);
-        StartCoroutine(_playerLookAt.LookAtRoutine(_lampAlarm.transform, 0.5f));
+
+        // Welcome message
+        yield return _levelManager.PlayerLookAt(_levelManager.lampAlarm.transform, 0.5f);
         yield return new WaitForSeconds(1);
-        StartCoroutine(_playerLookAt.LookAtRoutine(_TVScreen.transform, 0.5f));
-        yield return ShowMessageRoutine("WELCOME TO THE FAMILY");
-        yield return ShowMessageRoutine("YOUR SHIFT STARTS NOW", true);
+        StartCoroutine(_levelManager.PlayerLookAt(_levelManager.TVScreen.transform, 0.5f));
+        yield return _levelManager.ShowMessageRoutine("WELCOME TO THE FAMILY");
+
+        // First task
+        yield return _levelManager.ShowMessageRoutine("YOUR SHIFT STARTS NOW", true);
         yield return new WaitForSeconds(0.5f);
-        yield return _playerLookAt.LookAtRoutine(_lampAlarm.transform, 0.5f);
-        foreach (Lamp lamp in BeltLights)
-        {
-            lamp.TurnOn();
-            yield return new WaitForSeconds(0.5f);
-        }
-        yield return _playerLookAt.LookAtRoutine(_TVScreen.transform, 0.5f);
-        StartCoroutine(ShowMessageRoutine("TURN THE CONVEYOR BELT ON", true));
+        yield return _levelManager.PlayerLookAt(_levelManager.lampAlarm.transform, 0.5f);
+        yield return _levelManager.ToggleBeltLights(true);
+        yield return _levelManager.PlayerLookAt(_levelManager.TVScreen.transform, 0.5f);
+        StartCoroutine(_levelManager.ShowMessageRoutine("TURN THE CONVEYOR BELT ON", true));
         yield return new WaitForSeconds(1);
-        yield return _playerLookAt.LookAtRoutine(_lampAlarm.transform, 0.5f);
+        yield return _levelManager.PlayerLookAt(_levelManager.lampAlarm.transform, 0.5f);
         yield return new WaitForSeconds(1);
-        LeverLight.TurnOn();
-        _playerController.CanLook = _playerController.CanMove = true;
+        _levelManager.leverLight.TurnOn();
+        yield return new WaitForSeconds(0.5f);
+        _levelManager.playerController.CanLook = _levelManager.playerController.CanMove = true;
 
 
-        while (!_ConveyorBelt.IsOn)
-            yield return BlinkTVScreen();
+        while (!_levelManager.conveyorBelt.IsOn)
+            yield return _levelManager.BlinkTVScreen();
         yield return new WaitForSeconds(2);
-        yield return ShowMessageRoutine("GOOD    JOB");
+        yield return _levelManager.ShowMessageRoutine("GOOD    JOB");
 
 
         //_lampAlarm.TurnOn();
@@ -79,24 +64,5 @@ public class IntroductionSequence : MonoBehaviour
 
         yield break;
     }
-    IEnumerator BlinkTVScreen()
-    {
-        yield return new WaitForSeconds(0.5f);
-        _TVScreen.TurnOn();
-        yield return new WaitForSeconds(1);
-        _TVScreen.TurnOff();
-    }
-    IEnumerator ShowMessageRoutine(string txt, bool leaveOn = false)
-    {
-
-        _TVScreen.TurnOn();
-        _TVScreen.ChangeText(txt);
-        yield return new WaitForSeconds(2.5f);
-
-        if(!leaveOn)
-            _TVScreen.TurnOff();
-        yield return new WaitForSeconds(1);
-
-        yield break;
-    }
+  
 }
